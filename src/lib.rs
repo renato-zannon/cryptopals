7ea8e7392f8c06fc90fdae5d15bbd2da71236516
Renato Zannon <renato@rrsz.com.br>
2020-04-04 14:04:16 -0300

    Define a prelude for the shared library

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,9 @@ pub mod english_score;
 pub mod padding;
 pub mod string_wrap;
 pub mod xor;
+
+pub mod prelude {
+    pub use crate::encoding::{base64_to_bytes, block_pretty_print};
+    pub use crate::padding::{pkcs7_pad, pkcs7_unpad};
+    pub use crate::{aes, encoding, xor};
+}
