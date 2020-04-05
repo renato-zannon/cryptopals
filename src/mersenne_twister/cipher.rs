ff477aec5888f0088d17ae34e6767db87f6a59f6
Renato Zannon <renato@rrsz.com.br>
2020-04-05 17:41:41 -0300

    Allow mersenne cipher to use different seed types

--- src/mersenne_twister/cipher.rs
+++ src/mersenne_twister/cipher.rs
@@ -1,6 +1,8 @@
+use std::time::SystemTime;
+
 use super::MersenneTwister;
 
-pub fn encrypt(plaintext: &[u8], key: u16) -> Vec<u8> {
+pub fn encrypt<T: IntoSeed>(plaintext: &[u8], key: T) -> Vec<u8> {
     plaintext
         .iter()
         .zip(keystream(key))
@@ -10,11 +12,40 @@ pub fn encrypt(plaintext: &[u8], key: u16) -> Vec<u8> {
 
 pub use encrypt as decrypt;
 
-fn keystream(key: u16) -> impl Iterator<Item = u8> {
-    let first_byte = (key & 0xFF00) >> 8;
-    let second_byte = key & 0x00FF;
-
-    let seed = u32::from_be_bytes([0, 0, first_byte as u8, second_byte as u8]);
+fn keystream<T: IntoSeed>(key: T) -> impl Iterator<Item = u8> {
+    let seed = key.into_seed();
 
     MersenneTwister::new(seed).flat_map(|number| number.to_be_bytes().to_vec())
+}
+
+// Reuse core logic between different seed sources
+pub trait IntoSeed {
+    fn into_seed(self) -> u32;
+}
+
+impl IntoSeed for u16 {
+    fn into_seed(self) -> u32 {
+        let first_byte = (self & 0xFF00) >> 8;
+        let second_byte = self & 0x00FF;
+
+        u32::from_be_bytes([0, 0, first_byte as u8, second_byte as u8])
+    }
+}
+
+impl IntoSeed for u32 {
+    fn into_seed(self) -> u32 {
+        self
+    }
+}
+
+// Simplify using timestamp as seed
+impl IntoSeed for SystemTime {
+    fn into_seed(self) -> u32 {
+        let secs = self
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        secs as u32
+    }
 }
