5270dbca409dde75c5b403b8c2a882101fcd949e
Renato Zannon <renato@rrsz.com.br>
2020-04-13 23:04:09 -0300

    Create dh::Parameters to simplify usage

--- src/bin/set05-challenge33.rs
+++ src/bin/set05-challenge33.rs
@@ -1,36 +1,18 @@
-use rug::{Assign, Integer};
-
 use cryptopals::{dh, encoding::bytes_to_hex, utils::check_mark};
 
-const MODULUS: &str = "\
-  ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024\
-  e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd\
-  3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec\
-  6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f\
-  24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361\
-  c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552\
-  bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff\
-  fffffffffffff\
-";
-
-const BASE: u32 = 2;
-
 fn main() {
-    let mut modulus = Integer::new();
-    modulus.assign(Integer::parse_radix(MODULUS, 16).unwrap());
+    let parameters = dh::Parameters::default();
 
-    let base = Integer::from(BASE);
+    let (public_key_a, private_key_a) = parameters.generate_keypair();
+    let (public_key_b, private_key_b) = parameters.generate_keypair();
 
-    let (public_key_a, private_key_a) = dh::generate_keypair(&base, &modulus);
-    let (public_key_b, private_key_b) = dh::generate_keypair(&base, &modulus);
-
-    let session_key_a = dh::derive_session_key(&public_key_b, &private_key_a, &modulus);
+    let session_key_a = parameters.derive_session_key(&public_key_b, &private_key_a);
     println!(
         "Generated session key using priv A + pub B: {}",
         bytes_to_hex(&session_key_a.to_aes_key())
     );
 
-    let session_key_b = dh::derive_session_key(&public_key_a, &private_key_b, &modulus);
+    let session_key_b = parameters.derive_session_key(&public_key_a, &private_key_b);
     println!(
         "Generated session key using priv B + pub A: {}",
         bytes_to_hex(&session_key_b.to_aes_key())
--- src/dh.rs
+++ src/dh.rs
@@ -1,6 +1,6 @@
 use rand::prelude::*;
 use rug::integer::Order;
-use rug::{ops::PowAssign, Integer};
+use rug::{ops::PowAssign, Assign, Integer};
 
 use crate::sha1;
 
@@ -12,6 +12,51 @@ pub struct PrivateKey(Integer);
 
 #[derive(PartialEq, Eq, Debug)]
 pub struct SessionKey(Integer);
+
+const DEFAULT_MODULUS: &str = "\
+  ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024\
+  e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd\
+  3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec\
+  6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f\
+  24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361\
+  c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552\
+  bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff\
+  fffffffffffff\
+";
+
+const DEFAULT_BASE: u32 = 2;
+
+pub struct Parameters {
+    modulus: Integer,
+    base: Integer,
+}
+
+impl Parameters {
+    pub fn default() -> Self {
+        let mut modulus = Integer::new();
+        modulus.assign(Integer::parse_radix(DEFAULT_MODULUS, 16).unwrap());
+
+        let base = Integer::from(DEFAULT_BASE);
+
+        Self { modulus, base }
+    }
+
+    pub fn new(modulus: Integer, base: Integer) -> Self {
+        Self { modulus, base }
+    }
+
+    pub fn generate_keypair(&self) -> (PublicKey, PrivateKey) {
+        generate_keypair(&self.base, &self.modulus)
+    }
+
+    pub fn derive_session_key(
+        &self,
+        public_key: &PublicKey,
+        private_key: &PrivateKey,
+    ) -> SessionKey {
+        derive_session_key(public_key, private_key, &self.modulus)
+    }
+}
 
 impl SessionKey {
     pub fn to_aes_key(&self) -> Vec<u8> {
@@ -69,9 +114,10 @@ pub fn modexp(base: &Integer, exponent: &Integer, modulus: &Integer) -> Integer 
 
 pub fn random_integer<R: Rng>(modulus: &Integer, rng: &mut R) -> Integer {
     let bit_count = modulus.significant_bits() as usize;
+    let byte_count = modulus.significant_digits::<u8>();
 
     let mut result = Integer::with_capacity(bit_count);
-    let mut digits: Vec<u8> = vec![0; bit_count / 8];
+    let mut digits: Vec<u8> = vec![0; byte_count];
 
     loop {
         rng.fill(&mut digits[..]);
